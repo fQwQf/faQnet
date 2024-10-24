2a8fda4563d61aa4cee8e7df9e241112e7d18e71
fQwQf <supertjz123@foxmail.com>
2024-10-24 17:28:34 +0800

    效果也不好，全学到了最后一层的bias上，原因不明

--- demo/Obesity/obesity.cpp
+++ demo/Obesity/obesity.cpp
@@ -5,27 +5,27 @@ int main(){
 	std::vector<cv::Mat> target = faQnet::load_data("ObesityDataSet_raw_and_data_sinthetic.csv", 17, 17);
 	std::cout << "数据导入完成" << std::endl;
 	std::vector<int> layer_size = {16, 8 , 1};
-	std::vector<std::string> activation_function = {"softsign", "leaky_relu","none"};
+	std::vector<std::string> activation_function = {"leaky_relu", "elu","none"};
 	faQnet::net net(layer_size, activation_function);
 	std::cout << "网络初始化完成" << std::endl;
 
 	
 
-	net.init_bias("uniform", -0.1, 0.1);
-	net.init_weight("normal", 0, 0.1);
+	net.init_bias("uniform", -0.01, 0.01);
+	net.init_weight("normal", 0, 0.5);
 	std::cout << "矩阵初始化完成" << std::endl;
 
 	std::cout <<input.size() << std::endl;
 	net.preprocess_input(input);
 	std::cout << "输入数据预处理完成" << std::endl;
-	net.preprocess_target(target);
-	std::cout << "输出数据预处理完成" << std::endl;
+	//net.preprocess_target(target);
+	//std::cout << "输出数据预处理完成" << std::endl;
 
 	net.print_network();
 
 	for (int i = 0; i < input.size()-100; i++){
 		std::cout << "训练数据：" << i+1 <<"/" << input.size() << std::endl;
-		net.train(input[i], target[i], 0.000001 ,10,"mse");
+		net.train(input[i], target[i], 0.00001 ,100,"mse");
 	}
 
 	net.print_network();
