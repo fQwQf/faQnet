9758308b442ed6ca99bdf9e2bf33c40b95565b51
fQwQf <supertjz123@foxmail.com>
2024-10-24 14:57:05 +0800

    创建关于体重预测的demo，但性能不佳

---
+++ demo/Obesity/obesity.cpp
@@ -0,0 +1,39 @@
+#include "../../src/faQnet.cpp"
+
+int main(){
+	std::vector<cv::Mat> input = faQnet::load_data("ObesityDataSet_raw_and_data_sinthetic.csv",1, 16);
+	std::vector<cv::Mat> target = faQnet::load_data("ObesityDataSet_raw_and_data_sinthetic.csv", 17, 17);
+	std::cout << "数据导入完成" << std::endl;
+	std::vector<int> layer_size = {16, 8 , 1};
+	std::vector<std::string> activation_function = {"softsign", "leaky_relu","none"};
+	faQnet::net net(layer_size, activation_function);
+	std::cout << "网络初始化完成" << std::endl;
+
+	
+
+	net.init_bias("uniform", -0.1, 0.1);
+	net.init_weight("normal", 0, 0.1);
+	std::cout << "矩阵初始化完成" << std::endl;
+
+	std::cout <<input.size() << std::endl;
+	net.preprocess_input(input);
+	std::cout << "输入数据预处理完成" << std::endl;
+	net.preprocess_target(target);
+	std::cout << "输出数据预处理完成" << std::endl;
+
+	net.print_network();
+
+	for (int i = 0; i < input.size()-100; i++){
+		std::cout << "训练数据：" << i+1 <<"/" << input.size() << std::endl;
+		net.train(input[i], target[i], 0.000001 ,10,"mse");
+	}
+
+	net.print_network();
+
+	for (int i = input.size()-100; i < input.size(); i++){
+		std::cout << "预测数据：" << i-input.size()+101 <<"/" << 100 ;
+		std::cout << net.predict(input[i]) << std::endl;
+		std::cout << "实际数据：" << i-input.size()+101 <<"/" << 100 ;
+		std::cout << target[i] << std::endl;
+	}
+} 
