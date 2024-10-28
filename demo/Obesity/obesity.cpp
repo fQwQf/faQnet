59a98e51343eb7e0c213757fe513a6da0e7673df
fQwQf <supertjz123@foxmail.com>
2024-10-28 21:18:28 +0800

    fix: 修改关于肥胖的数据，使其性能更好

--- demo/Obesity/obesity.cpp
+++ demo/Obesity/obesity.cpp
@@ -1,11 +1,11 @@
 #include "../../src/faQnet.h"
 
 int main(){
-	std::vector<cv::Mat> input = faQnet::load_data("ObesityDataSet_raw_and_data_sinthetic.csv",1, 16);
-	std::vector<cv::Mat> target = faQnet::load_data("ObesityDataSet_raw_and_data_sinthetic.csv", 17, 17);
+	std::vector<cv::Mat> input = faQnet::load_data("ObesityDataSet_raw_and_data_sinthetic.csv",1, 14);
+	std::vector<cv::Mat> target = faQnet::load_data("ObesityDataSet_raw_and_data_sinthetic.csv", 15, 20);
 	std::cout << "数据导入完成" << std::endl;
-	std::vector<int> layer_size = {16, 1};
-	std::vector<std::string> activation_function = {"leaky_relu", "elu"};
+	std::vector<int> layer_size = {14,20, 6};
+	std::vector<std::string> activation_function = {"leaky_relu","sigmoid", "elu"};
 	faQnet::net net(layer_size, activation_function);
 	std::cout << "网络初始化完成" << std::endl;
 
@@ -13,19 +13,18 @@ int main(){
 
 	net.init_bias("uniform", -0.01, 0.01);
 	net.init_weight("normal", 0, 0.5);
-	std::cout << "\" << std::endl;
 
 	std::cout <<input.size() << std::endl;
 	net.normalize_preprocess_input(input);
 	std::cout << "输入数据预处理完成" << std::endl;
-	//net.normalize_preprocess_target(target);
-	//std::cout << "输出数据预处理完成" << std::endl;
+	net.normalize_preprocess_target(target);
+	std::cout << "输出数据预处理完成" << std::endl;
 
 	net.print_net();
 
 	for (int i = 0; i < input.size()-100; i++){
 		std::cout << "训练数据：" << i+1 <<"/" << input.size() << std::endl;
-		net.train(input[i], target[i], 0.000005 ,1000,"mse");
+		net.train(input[i], target[i], 0.0005 ,50,"mse");
 	}
 
 	net.print_net();
